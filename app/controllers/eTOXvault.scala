c823f076fe392e136d071b2de26d2b9a6d0e6ad3
Oriol López-Massaguer <oriol.lopez@gmail.com>
2017-06-01 17:10:08 +0200

    etox_vault_cache_1

--- app/controllers/eTOXvault.scala
+++ app/controllers/eTOXvault.scala
@@ -12,17 +12,23 @@ object eTOXvault {
   def getModelInfo(tag: String) = {
 
     val model_json_url = "http://lamia.upf.edu/modelinfo/?modeltag=" + tag + "&authkey=7b80f381248245c4&provider=FIMIM"
+    println("Model info etox vault: ")
+    println(model_json_url)
     implicit val context = scala.concurrent.ExecutionContext.Implicits.global
 
     val futureResult_url = WS
       .url(model_json_url)
 
-    val futureResult = futureResult_url.get()
-    val response = Await.result(futureResult, 10 seconds)
+    val jso = try {
+      val futureResult = futureResult_url.get()
+      val response = Await.result(futureResult, 10 seconds)
 
-    val jso = Json.parse(response.body).as[JsObject]
-    val mp = for ((field, value) <- jso.value if (field != "software"))
-      yield ((field, value.as[JsString].value))
+      val jso = Json.parse(response.body).as[JsObject]
+      //val mp = for ((field, value) <- jso.value if (field != "software"))        yield ((field, value.as[JsString].value))
+      jso
+    } catch {
+      case e: Exception => play.api.libs.json.Json.toJson(Map("error" -> "etoxault erroneus response")).as[JsObject]
+    }
 
     jso
   }
---
+++ app/model/etoxvaultCache.scala
@@ -0,0 +1,18 @@
+package model
+
+object etoxvaultCache {
+
+  val fileNameCache = controllers.Application.envoy_ws_home + "/etoxvault_cache.txt"
+  def load_cache_from_ws= {
+
+    val data = for ((k, (modelid, tag, _, _)) <- controllers.eTOXlab.models)
+      yield (
+      Map("tag" -> tag,
+        "info" -> controllers.eTOXvault.getModelInfo(tag.replaceAll(" ","%20")).toString))
+        
+    val df=models.dataframe.DataFrame(data.toList)
+    df.toText(fileNameCache)
+  }
+  
+  laz
+}
